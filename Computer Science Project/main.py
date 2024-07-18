a51a93920e030902a68ab45e4ba4a59bab1b8a90
Samwel-SE <99136901+Samwel-SE@users.noreply.github.com>
2024-07-18 18:30:36 +0100

    Add files via upload
    First version of game uploaded. Collision still needs patching

---
+++ Computer Science Project/main.py
@@ -0,0 +1,139 @@
+import random
+import sys
+
+import pygame
+from pygame.locals import QUIT
+
+#imports script I wrote for procedurally generation
+import perlinnoise
+
+#game object class, to be inherited by player and projectile class
+class GameObject():
+
+  def __init__(self, x, y, width, height, colour):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.colour = colour
+
+  def draw(self):
+    pygame.draw.rect(DISPLAYSURF, self.colour, pygame.Rect(self.x, self.y, self.width, self.height))
+
+  def collision(self, obj2):
+    if (self.x < obj2.x + obj2.width and self.x + self.width > obj2.x) or (self.y + self.height > obj2.y and self.y + self.height < obj2.y + obj2.height):
+      return True
+
+#map class
+class Map():
+
+  def __init__(self, screen_width, smoothness, colour):
+    self.screen_width = screen_width
+    self.smoothness = smoothness
+    self.colour = colour
+    self.y = perlinnoise.generate(self.screen_width, self.smoothness)
+    self.map_pieces = []
+
+    for i in range(1600):
+      self.map_pieces.append(GameObject(i, self.y[i], 1, 700, self.colour))
+
+  def draw(self):
+    for i in range(1600):
+      self.map_pieces[i].draw()
+
+
+
+#player class inherits gameobject class
+class Player(GameObject):                         
+
+  def __init__(self, x, y, width, height, colour):
+    super().__init__(x, y, width, height, colour)
+    self.movement = [False, False]
+    self.jumping = False
+    self.jump_vel = 12
+    self.jump_height = self.jump_vel
+
+  #gets keys currently pressed so player character can move
+  def inputs(self):                                         
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_d:
+        self.movement[0] = True
+      if event.key == pygame.K_a:
+        self.movement[1] = True
+      if event.key == pygame.K_w and self.jumping == False:
+        self.jumping = True
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_d:
+          self.movement[0] = False
+        if event.key == pygame.K_a:
+          self.movement[1] = False
+
+  #player jump
+  def jump(self):
+    if self.jumping:
+      self.y -= self.jump_vel
+      self.jump_vel -= 1  
+
+    #while player has not pressed jump key they will constantly move downwards as to not break game
+    else:
+      self.y += 10
+
+
+#pygame initialisation
+pygame.init()                                            
+background = (255, 255, 255)
+DISPLAYSURF = pygame.display.set_mode((1500, 800))
+DISPLAYSURF.fill(background)                                                              
+
+#colours 
+black = (0, 0, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+
+#creates procedurally generated terrain, parameters are: screen width and smoothness of terrain
+terr = Map(1600, random.randint(140, 170), black)  
+
+#game objects
+l_wall = GameObject(0, 0, 1, 800, blue)
+player1 = Player(300, 300, 5, 5, red,)
+
+Clock = pygame.time.Clock()
+pygame.display.set_caption('Tanks')      
+
+#game loop
+while True:                                                                 
+  DISPLAYSURF.fill(background)   
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+    player1.inputs()
+
+  #player movement
+  player1.jump()
+  if player1.movement[0]:                                                  
+    player1.x += 3
+  if player1.movement[1]:
+    player1.x -= 3
+
+
+  #player collision
+  if player1.collision(terr.map_pieces[player1.x]): 
+    player1.y = terr.map_pieces[player1.x].y -5
+    player1.jumping = False
+    player1.jump_vel = player1.jump_height
+  
+
+  #if player1.collision(terr.map_pieces[player1.x + 5]) or player1.collision(terr.map_pieces[player1.x -5]):
+    #print("testing")
+    #player1.x = terr.map_pieces[player1.x].x
+
+
+  
+  #game objects drawn here
+  terr.draw()
+  player1.draw()
+  l_wall.draw()
+  pygame.display.update()
+  Clock.tick(60)  
---
+++ Computer Science Project/perlinnoise.py
@@ -0,0 +1,22 @@
+import random
+
+
+def randomise(terms):
+  for i in range(len(terms)):
+    terms[i] = random.randint(
+        300, 1000)  #these parameters control y variable of terrain
+
+
+def smooth(terms):  #smooths terrain by setting y variable equal to average of itself and adjacent y variables
+  terms[0] = (terms[0] + terms[1]) // 2
+  for i in range(1, len(terms) - 2):
+    terms[i] = (terms[i - 1] + terms[i] + terms[i + 1]) // 3
+  terms[len(terms) - 1] = (terms[len(terms) - 2] + terms[len(terms) - 3]) // 2
+
+
+def generate(width, smoothness):  #takes parameters of width of screen and how many times they want smoothness function to run
+  terr = [0] * width
+  randomise(terr)
+  for i in range(smoothness):
+    smooth(terr)
+  return terr
